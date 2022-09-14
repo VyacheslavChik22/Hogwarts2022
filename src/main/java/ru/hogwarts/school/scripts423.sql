7ce76bb00ec57f1735a4cc2d638460fd2d702f3e
asyof <asyofka@mail.ru>
2022-09-14 20:59:20 +0300

    Hogwarts SQL, table2

---
+++ src/main/java/ru/hogwarts/school/scripts423.sql
@@ -0,0 +1,11 @@
+SELECT student.name, student.age,faculty.name
+FROM student
+INNER JOIN faculty ON student.faculty_id = faculty.id;
+
+
+
+SELECT avatar.id,student.name
+FROM avatar
+LEFT JOIN student ON avatar.student_id = student.id;
+
+
